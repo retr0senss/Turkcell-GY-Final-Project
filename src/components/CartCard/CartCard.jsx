1f266b82578845da805412495cea4e1e12aed751
Burak Coskun <92813347+retr0senss@users.noreply.github.com>
2023-07-01 14:11:31 +0300

    Cart Updated

--- src/components/CartCard/CartCard.jsx
+++ src/components/CartCard/CartCard.jsx
@@ -1,6 +1,10 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const CartCard = ({ product, handleRemove, handleQuantity }) => {
+  const [quantity, setQuantity] = useState(product.quantity);
+  const navigate = useNavigate();
   return (
     <div className="row">
       <div className="col-12">
@@ -10,34 +14,52 @@ const CartCard = ({ product, handleRemove, handleQuantity }) => {
               src={product.image}
               alt="Product"
               className="img-fluid rounded"
+              onClick={() => navigate(`/product-detail/${product.id}`)}
             />
           </div>
           <div className="col-12 col-lg-10 d-flex align-items-center ">
             <div className="row justify-content-between w-100">
               <div className="col-12 col-lg-6">
-                <h3>{product.title}</h3>
+                <h3 onClick={() => navigate(`/product-detail/${product.id}`)}>
+                  {product.title}
+                </h3>
                 <p>Price: ${product.price}</p>
               </div>
               <div className="col-12 col-lg-6 ">
                 <div className="row ">
                   <div className="col-12 col-lg-6 d-flex gx-5">
-                    <button className="btn btn-primary">-</button>
+                    <button
+                      className="btn btn-primary"
+                      onClick={() => {
+                        if (quantity === 1) handleRemove(product.id);
+                        setQuantity(quantity - 1);
+                        handleQuantity(product.id, quantity - 1);
+                      }}
+                    >
+                      -
+                    </button>
                     <input
                       type="number"
-                      className="form-control mx-4"
-                      value={product.quantity}
-                      readOnly
+                      className="form-control mx-3 w-100 text-center"
+                      value={quantity}
+                      onChange={(e) => {
+                        setQuantity(parseInt(e.target.value));
+                        handleQuantity(product.id, parseInt(e.target.value));
+                      }}
                     />
                     <button
                       className="btn btn-primary"
-                      onClick={() => handleQuantity(product.id, 1)}
+                      onClick={() => {
+                        setQuantity(quantity + 1);
+                        handleQuantity(product.id, quantity + 1);
+                      }}
                     >
                       +
                     </button>
                   </div>
                   <div className="col-12 col-lg-6">
                     <button
-                      className="btn btn-danger"
+                      className="btn btn-secondary"
                       onClick={() => handleRemove(product.id)}
                     >
                       Remove
--- src/views/Cart/Cart.jsx
+++ src/views/Cart/Cart.jsx
@@ -1,41 +1,57 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useState, useEffect } from "react";
 import CartCard from "../../components/CartCard/CartCard";
 import ProductCard from "../../components/ProductCard/ProductCard";
-import { useDispatch } from "react-redux";
 import { removeFromCart, clearCart } from "../../redux/slices/cartSlice";
 import { Carts, Products } from "../../services/api";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Cart = () => {
   const [total, setTotal] = useState(0);
   const dispatch = useDispatch();
   const products = useSelector((state) => state.product.products);
   const cartDatabase = useSelector((state) => state.cart.cart);
-  const cartProducts = products
-    .map((product) => {
-      const cartProduct = cartDatabase[0]?.products.find(
-        (cartProduct) => cartProduct.productId === product.id
-      );
-      if (cartProduct) {
-        return {
-          ...product,
-          quantity: cartProduct.quantity,
-        };
-      }
-      return null;
-    })
-    .filter((product) => product !== null);
+  const [cartProducts, setCartProducts] = useState([]);
+  const [randomProducts, setRandomProducts] = useState([]);
 
-  const randomProducts = products
-    .filter((products) => {
+  useEffect(() => {
+    const updatedCartProducts = products
+      .map((product) => {
+        const cartProduct = cartDatabase[0]?.products.find(
+          (cartProduct) => cartProduct.productId === product.id
+        );
+        if (cartProduct) {
+          return {
+            ...product,
+            quantity: cartProduct.quantity,
+          };
+        }
+        return null;
+      })
+      .filter((product) => product !== null);
+
+    setCartProducts(updatedCartProducts);
+  }, [products, cartDatabase]);
+
+  useEffect(() => {
+    const filteredProducts = products.filter((product) => {
       const cartProductIds = cartProducts.map((product) => product.id);
-      return !cartProductIds.includes(products.id);
-    })
-    .sort(() => Math.random() - 0.5);
+      return !cartProductIds.includes(product.id);
+    });
 
-  const handleRemove = (id) => {
+    const shuffledProducts = filteredProducts.sort(() => Math.random() - 0.5);
+
+    setRandomProducts(shuffledProducts.slice(0, 4));
+  }, [products, cartProducts]);
+
+  const fetchProductStock = async (productId) => {
+    const product = await Products.getOne(productId);
+    return product.rating.count;
+  };
+
+  const handleRemove = async (id) => {
     dispatch(removeFromCart(id));
-    cartProducts.filter((product) => product.id !== id);
     const newCartDatabase = cartDatabase[0].products.filter(
       (product) => product.productId !== id
     );
@@ -43,48 +59,87 @@ const Cart = () => {
       ...cartDatabase[0],
       products: newCartDatabase,
     });
+
+    setCartProducts(cartProducts.filter((product) => product.id !== id));
   };
 
-  const handleQuantity = (id, quantity) => {
-    /*  const newCartDatabase = cartDatabase[0].products.map((product) => {
-      if (product.productId === id) {
-        return {
-          ...product,
-          quantity: product.quantity + quantity,
-        };
-      }
-    }); */
-
-    cartProducts.map((product) => {
-      if (product.id === id) {
-        product.quantity = product.quantity + quantity;
-      }
-    });
-
-    /*  Carts.editOne(cartDatabase[0].id, {
-      ...cartDatabase[0],
-      products: newCartDatabase,
-    }); */
-  };
-
-  const handleCheckout = () => {
-    cartProducts.map((cartProduct) => {
-      const newStock =
-        products.find((product) => product.id === cartProduct.id).rating.count -
-        cartProduct.quantity;
-      delete cartProduct.quantity;
-      Products.editOne(cartProduct.id, {
-        ...cartProduct,
-        rating: {
-          rate: cartProduct.rating.rate,
-          count: newStock,
-        },
+  const handleQuantity = async (id, quantity) => {
+    const stock = await fetchProductStock(id);
+    if (quantity <= stock) {
+      const newCartProducts = cartProducts.map((product) => {
+        if (product.id === id) {
+          return {
+            ...product,
+            quantity: quantity,
+          };
+        }
+        return product;
       });
-    });
+
+      setCartProducts(newCartProducts);
+
+      const newCartDatabase = cartDatabase[0].products.map((product) => {
+        if (product.productId === id) {
+          return {
+            ...product,
+            quantity: quantity,
+          };
+        }
+        return product;
+      });
+
+      Carts.editOne(cartDatabase[0].id, {
+        ...cartDatabase[0],
+        products: newCartDatabase,
+      });
+    } else {
+      if (stock === 0) {
+        toast.error("This product is out of stock.");
+        return;
+      }
+
+      const stockMessage = `You are trying to add ${quantity} items, but only ${stock} items are available in stock.`;
+      toast.error(stockMessage);
+    }
+  };
+
+  const handleCheckout = async () => {
+    let outOfStockProducts = [];
+
+    for (const cartProduct of cartProducts) {
+      const productStock = await fetchProductStock(cartProduct.id);
+      const newStock = productStock - cartProduct.quantity;
+
+      if (newStock < 0) {
+        outOfStockProducts.push(cartProduct.title);
+      } else {
+        delete cartProduct.quantity;
+        Products.editOne(cartProduct.id, {
+          ...cartProduct,
+          rating: {
+            ...cartProduct.rating,
+            count: newStock,
+          },
+        });
+
+        if (newStock === 0 && cartProduct.quantity > 0) {
+          toast.warn(`${cartProduct.title} is now out of stock!`);
+        }
+      }
+    }
+
+    if (outOfStockProducts.length > 0) {
+      const outOfStockMessage = outOfStockProducts.join(", ");
+      toast.error(
+        `The following products are out of stock: \n${outOfStockMessage}`
+      );
+      return;
+    }
+
     emptyCart();
   };
 
-  const emptyCart = () => {
+  const emptyCart = async () => {
     Carts.removeCart(cartDatabase[0].id);
     dispatch(clearCart());
   };
@@ -94,7 +149,7 @@ const Cart = () => {
       (total, product) => total + product.price * product.quantity,
       0
     );
-    setTotal(total);
+    setTotal(total.toFixed(2));
   }, [cartProducts]);
 
   return (
@@ -146,7 +201,7 @@ const Cart = () => {
           <div className="row mt-5">
             <h3>Recommended Products</h3>
             <hr />
-            {randomProducts?.slice(0, 4).map((product) => (
+            {randomProducts.map((product) => (
               <div className="col-12 col-lg-3" key={product.id}>
                 <ProductCard product={product} />
               </div>
@@ -154,6 +209,7 @@ const Cart = () => {
           </div>
         </div>
       </div>
+      <ToastContainer />
     </div>
   );
 };
